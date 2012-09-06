b79c109d9309491cb53b813c9ba1cb471cbfe937
Kyle Wiest <kyle.wiest@gmail.com>
2012-09-06 16:58:06 -0700

    Remove auth constraint and just handle simple logic in view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ MyTweets::Application.routes.draw do
   match '/sign_in'  => 'sessions#create', via: 'post'
   match '/sign_out' => 'sessions#destroy', via: 'delete'
 
-  root to: 'index#my_tweets', constraints: AuthenticationConstraint
   root to: 'index#index'
 end
--- lib/authentication_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AuthenticationConstraint
-  def self.matches?(request)
-    user_id = request.env['action_dispatch.request.unsigned_session_cookie']['user_id']
-    user_id
-  end
-end
