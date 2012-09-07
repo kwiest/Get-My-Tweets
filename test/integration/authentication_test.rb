a97d20b384cc2f9ac9bd138de83ec3220198ba2f
Kyle Wiest <kyle.wiest@gmail.com>
2012-09-06 17:14:25 -0700

    Auth integration test passing

--- test/integration/authentication_test.rb
+++ test/integration/authentication_test.rb
@@ -1,15 +1,32 @@
 require 'test_helper'
 
 class AuthenticationTest < ActionDispatch::IntegrationTest
-  def test_sign_in
-    user = User.create(email: 'me@example.com', password: 'secret')
+  def setup
+    @user = User.create email: 'kyle@example.com', password: 'secret'
+  end
 
+  def sign_in(user)
     visit root_path
-
-    fill_in 'email', with: 'me@example.com'
-    fill_in 'password', with: 'secret'
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
     click_button 'Sign in'
+  end
 
-    assert contains?('Signed in!')
+  def test_sign_in
+    sign_in @user
+
+    assert has_content?(@user.email), 'Displays current user email address'
+    assert has_content?('sign out'), 'Has a link to sign out'
+    refute has_css?('form#user-session'), 'Does not have session form'
+  end
+
+  def test_sign_out
+    reset_session!
+    sign_in @user
+    click_link 'sign out'
+
+    assert has_css?('form#user-session'), 'Should have form to sign in'
+    refute has_content?(@user.email), 'Does not display current user email address'
+    refute has_content?('sign out'), 'Does not have a link to sign out'
   end
 end
