fc7ac68bbdb88422f994576d551a59c299ec5fb8
Kyle Wiest <kyle.wiest@gmail.com>
2012-09-25 19:48:34 -0700

    Another spike with headers set on all requests

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,32 @@
 class TweetsController < ApplicationController
   respond_to :json
-  skip_before_filter :verifty_authenticity_token
-
-  REQUEST_METHODS = {
-    'GET'     => :authenticate_user,
-    'OPTIONS' => :set_cors_headers
-  }
+  before_filter :determine_cors_request_type
 
   def index
-    send REQUEST_METHODS[request.method]
   end
 
 
   private
+
+  def determine_cors_request_type
+    case request.method
+    when 'GET'
+      set_cors_headers
+      authenticate_user
+      send_twitter_json_response
+    when 'OPTIONS'
+      set_cors_headers
+      render json: '{}'
+    else
+      head :unauthorized
+    end
+  end
+
+  def set_cors_headers
+    response.headers['Access-Control-Allow-Headers'] = '*, Authorization, X-Requested-With, X-Prototype-Version, X-CRSF-Token, Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    response.headers['Access-Control-Allow-Origin']  = '*'
+  end
 
   def authenticate_user
     authenticate_with_http_basic do |username, password|
@@ -21,20 +35,12 @@ class TweetsController < ApplicationController
 
     head :unauthorized and return unless @api_key
     @user = @api_key.user
-    send_json_response
   end
 
-  def set_cors_headers
-    response.headers['Access-Control-Allow-Headers'] = '*, Authorization, X-Requested-With, X-Prototype-Version, X-CRSF-Token, Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-    response.headers['Access-Control-Allow-Origin']  = '*'
-    render json: '{}'
-  end
-
-  def send_json_response
+  def send_twitter_json_response
     authorization = @user.authorizations.find_by_username params[:username]
     options = params.fetch :options, {}
-    timeline = authorization.make_twitter_request :user_timeline, options
-    render json: timeline
+    twitter_json = authorization.make_twitter_request :user_timeline, options
+    render json: twitter_json
   end
 end
